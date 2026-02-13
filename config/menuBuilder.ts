b668ac5258a0d0b06ea664621e8f8d8d545bb632
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-02-13 21:16:13 +0100

    menu

--- config/menuBuilder.ts
+++ config/menuBuilder.ts
@@ -1,11 +1,11 @@
-import type { MenuBuilder, MenuItemConfig } from '@menu/index'
+import { MenuBuilder, type MenuItemConfig } from '@menu/index'
 import { FileText, Layout, Layers } from 'lucide-vue-next'
 
 /**
  * CMS Menu Builder
  * Builds the CMS management menu structure
  */
-export class CmsMenuBuilder implements MenuBuilder {
+export class CmsMenuBuilder extends MenuBuilder {
   build(menu: MenuItemConfig, menuName: string): MenuItemConfig {
     if (menuName !== 'admin') {
       return menu
@@ -35,18 +35,7 @@ export class CmsMenuBuilder implements MenuBuilder {
       ]
     }
 
-    // Add to menu children
-    if (!menu.children) {
-      menu.children = []
-    }
-    menu.children.push(cmsSection)
-
-    // Sort children by order
-    menu.children.sort((a, b) => {
-      const orderA = a.order ?? Number.MAX_SAFE_INTEGER
-      const orderB = b.order ?? Number.MAX_SAFE_INTEGER
-      return orderA - orderB
-    })
+    this.addMenuItem(menu, cmsSection)
 
     return menu
   }
